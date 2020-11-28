662513db9f9775857cbcdf6ab8a1166e1244ea74
andres velasco <velasco.andrs@gmail.com>
2020-11-28 12:34:51 -0500

    pull to refresh

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
 import 'package:secret_chat_mobile/models/usuario.dart';
 
 class UsuariosPage extends StatefulWidget {
@@ -7,59 +10,92 @@ class UsuariosPage extends StatefulWidget {
 }
 
 class _UsuariosPageState extends State<UsuariosPage> {
+  RefreshController _refreshController = new RefreshController();
+
   final usuarios = [
     Usuario(uid: '1', nombre: 'Maria', email: 'marial@test.com', online: true),
     Usuario(
         uid: '2', nombre: 'Fernando', email: 'marial@test.com', online: true),
-    Usuario(uid: '3', nombre: 'Andres', email: 'marial@test.com', online: false),
+    Usuario(
+        uid: '3', nombre: 'Andres', email: 'marial@test.com', online: false),
     Usuario(uid: '4', nombre: 'Pepe', email: 'marial@test.com', online: true),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Mi Nombre',
-            style: TextStyle(color: Colors.black54),
-          ),
-          elevation: 1,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {},
-          ),
-          actions: [
-            Container(
-              margin: EdgeInsets.only(right: 10),
-              child: Icon(
-                Icons.check_circle,
-                color: Colors.blue[400],
-              ),
-              // child: Icon(
-              //   Icons.offline_bolt,
-              //   color: Colors.red,
-              // ),
-            ),
-          ],
+      appBar: AppBar(
+        title: Text(
+          'Mi Nombre',
+          style: TextStyle(color: Colors.black54),
         ),
-        body: ListView.separated(
-          physics: BouncingScrollPhysics(),
-          itemBuilder: (_, i) => ListTile(
-            title: Text(usuarios[i].nombre),
-            leading: CircleAvatar(
-              child: Text(usuarios[i].nombre.substring(0, 2)),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.exit_to_app),
+          onPressed: () {},
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.check_circle,
+              color: Colors.blue[400],
             ),
-            trailing: Container(
-              width: 10,
-              height: 10,
-              decoration: BoxDecoration( 
-                color: usuarios[i].online ? Colors.green[300] : Colors.red,
-                borderRadius: BorderRadius.circular(100),
-              ),
-            ),
+            // child: Icon(
+            //   Icons.offline_bolt,
+            //   color: Colors.red,
+            // ),
           ),
-          separatorBuilder: (_, i) => Divider(),
-          itemCount: this.usuarios.length,
-        ));
+        ],
+      ),
+      body: SmartRefresher(
+        controller: this._refreshController,
+        child: _buildUsersListView(),
+        enablePullDown: true,
+        onRefresh: this._cargarUsuarios,
+      ),
+    );
+  }
+
+  ListView _buildUsersListView() {
+    return ListView.separated(
+      physics: BouncingScrollPhysics(),
+      itemBuilder: (_, i) => _buildUserListTile(usuarios[i]),
+      separatorBuilder: (_, i) => Divider(),
+      itemCount: this.usuarios.length,
+    );
+  }
+
+  ListTile _buildUserListTile(Usuario usuario) {
+    return ListTile(
+      title: Text(usuario.nombre),
+      subtitle: Text(usuario.nombre),
+      leading: CircleAvatar(
+        child: Text(
+          usuario.nombre.substring(0, 2),
+        ),
+        backgroundColor: Colors.blue[100],
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color: usuario.online ? Colors.green[300] : Colors.red,
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
+    );
+  }
+
+  _cargarUsuarios() async {
+    await Future.delayed(
+      Duration(milliseconds: 1000),
+    );
+    this.usuarios.add(
+          Usuario(
+              uid: '4', nombre: 'Pepe', email: 'marial@test.com', online: true),
+        );
+    setState(() {});
+    _refreshController.refreshCompleted();
   }
 }
