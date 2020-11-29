6a0eb668885a75d5926724652946a1c81e348b85
andres velasco <velasco.andrs@gmail.com>
2020-11-28 20:08:32 -0500

    caja de texto

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _textController = new TextEditingController();
+  final FocusNode _focusNode = new FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,10 +53,62 @@ class ChatPage extends StatelessWidget {
             Container(
               color: Colors.white,
               height: 100,
+              child: this._inputChat(),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: this._textController,
+                onSubmitted: this._manejarEnvioMensaje,
+                onChanged: (String texto) {},
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Escribe tu mensaje',
+                ),
+                focusNode: this._focusNode,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: this._botonEnviar(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _botonEnviar() {
+    if (Platform.isIOS) {
+      return CupertinoButton(
+          child: Text('Enviar'), onPressed: this._enviarMensaje);
+    } else {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 4.0),
+        child: IconButton(
+          onPressed: this._enviarMensaje,
+          icon: Icon(
+            Icons.send,
+            color: Colors.blue[400],
+          ),
+        ),
+      );
+    }
+  }
+
+  void _enviarMensaje() {}
+  void _manejarEnvioMensaje(String mensaje) {
+    this._textController.clear();
+    this._focusNode.requestFocus();
   }
 }
