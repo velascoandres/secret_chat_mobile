b1daa674538f51f9609d616eb0efe36cec6cb5c6
andres velasco <velasco.andrs@gmail.com>
2020-12-08 23:37:52 -0500

    install provider and http

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:secret_chat_mobile/routes/routes.dart';
- 
+import 'package:secret_chat_mobile/services/auth_service.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Caht App',
-      initialRoute: 'chat',
-      routes: appRoutes,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthService(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Caht App',
+        initialRoute: 'login',
+        routes: appRoutes,
+      ),
     );
   }
 }
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+class AuthService with ChangeNotifier {
+  // final usuario;
+}
